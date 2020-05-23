c64ae564971f7122c128e48ad539397334a6a965
James Dimonaco <James@Jamess-MacBook-Pro.local>
2020-05-23 20:13:38 +0100

    button colour chnages not fixed yet

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { IonToolbar, IonTitle, IonButton, IonHeader } from "@ionic/react";
-
+import '../pages/Home.css';
 
 const Header: React.FC = () => {
     return(
         <IonHeader>
         <IonToolbar>
           <IonTitle>
-            <IonButton className="leftButterCenter" routerLink='/home'>Home</IonButton>
-            <IonButton className="Center" routerLink='/Collections'>Collections</IonButton>
-            <IonButton className="Center">Prints</IonButton>
-            <IonButton className="Center">Contact</IonButton>
+            <IonButton id="buttons" className="leftButterCenter" routerLink='/home'>Home</IonButton>
+            <IonButton id="buttons" className="Center" routerLink='/Collections'>Collections</IonButton>
+            <IonButton id="buttons" className="Center">Prints</IonButton>
+            <IonButton id="buttons" className="Center">Contact</IonButton>
           </IonTitle>
         </IonToolbar>
         </IonHeader>
