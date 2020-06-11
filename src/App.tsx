e54168be82a5fd1559a837a22d76341f629c60c1
James Dimonaco <jamesdimonaco@MacBook-Pro.lan>
2020-06-11 15:15:33 +0100

    update on design

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
-import Collections from './pages/Collections';
+
 
 const App: React.FC = () => (
   <IonApp>
@@ -30,7 +30,7 @@ const App: React.FC = () => (
       <IonRouterOutlet>
         <Route path="/home" component={Home} exact={true} />
         <Route exact path="/" render={() => <Redirect to="/home" />} />
-        <Route path="/Collections" component={Collections} />
+       
      
        
       </IonRouterOutlet>
