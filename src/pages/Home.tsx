9b7db93dd98241ac491436a17d8a5beaa89ddb6b
James Dimonaco <jamesdimonaco@hotmail.com>
2020-06-21 23:30:45 +0100

    alt fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -17,6 +17,7 @@ const Home: React.FC = () => {
           <img
             className='bertFace'
             src='https://static.wixstatic.com/media/0dfd41_70930db19992466189109c270dd826f1~mv2.jpg/v1/fill/w_1498,h_1662,al_c,q_90,usm_0.66_1.00_0.01/face%20spalt.webp'
+            alt='Albert'
           />
         </div>
         <div></div>
--- src/pages/Portraits.tsx
+++ src/pages/Portraits.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Home.css'
 import Header from '../components/Header'
-import { IonPage, IonContent, IonHeader, IonButton } from '@ionic/react'
+import { IonPage, IonContent, IonHeader, IonToggle } from '@ionic/react'
 
 const Portraits: React.FC = () => {
   return (
@@ -12,64 +12,108 @@ const Portraits: React.FC = () => {
             <Header />
           </div>
         </IonHeader>
+        <IonToggle></IonToggle>
         <div className=''>
           <img
             id='one'
             src='https://cdn.discordapp.com/attachments/533906819324837944/720639819151048834/james2_done.jpg'
+            alt=''
           />
         </div>
         <div className='flex'>
           <div className='' id='two'>
-            <img src='https://cdn.discordapp.com/attachments/533906819324837944/720639799542677584/jamesdone1.jpg' />
+            <img
+              src='https://cdn.discordapp.com/attachments/533906819324837944/720639799542677584/jamesdone1.jpg'
+              alt=''
+            />
           </div>
 
           <div id='three'>
-            <img src='https://cdn.discordapp.com/attachments/533906819324837944/720640374858842134/james4.JPG' />
+            <img
+              src='https://cdn.discordapp.com/attachments/533906819324837944/720640374858842134/james4.JPG'
+              alt=''
+            />
           </div>
         </div>
         <div id='four'>
-          <img src='https://cdn.discordapp.com/attachments/533906819324837944/720639930300104715/jam88.jpg' />
+          <img
+            src='https://cdn.discordapp.com/attachments/533906819324837944/720639930300104715/jam88.jpg'
+            alt=''
+          />
         </div>
         <div className='flex'>
           <div id='five'>
-            <img src='https://storage.googleapis.com/web-images-for-bert/Edited%20all/jamie%20Portraits%20pic%205' />
+            <img
+              src='https://storage.googleapis.com/web-images-for-bert/Edited%20all/jamie%20Portraits%20pic%205'
+              alt=''
+            />
           </div>
           <div id='six'>
-            <img src='https://storage.googleapis.com/web-images-for-bert/Edited%20all/Jamie%20Portraits%20pic%206.JPG' />
+            <img
+              src='https://storage.googleapis.com/web-images-for-bert/Edited%20all/Jamie%20Portraits%20pic%206.JPG'
+              alt=''
+            />
           </div>
         </div>
         <div className='flex'>
           <div id='six'>
-            <img src='https://cdn.discordapp.com/attachments/533906819324837944/720639880945729706/jamie_instasend_.jpg' />
+            <img
+              src='https://cdn.discordapp.com/attachments/533906819324837944/720639880945729706/jamie_instasend_.jpg'
+              alt=''
+            />
           </div>
           <div id='five'>
-            <img src='https://cdn.discordapp.com/attachments/533906819324837944/720639927573807184/jam3.jpg' />
+            <img
+              src='https://cdn.discordapp.com/attachments/533906819324837944/720639927573807184/jam3.jpg'
+              alt=''
+            />
           </div>
         </div>
 
         <div id='seven'>
-          <img src='https://cdn.discordapp.com/attachments/533906819324837944/723537677655474256/unknown.png' />
+          <img
+            src='https://cdn.discordapp.com/attachments/533906819324837944/723537677655474256/unknown.png'
+            alt=''
+          />
         </div>
 
         <div className='flexpic'>
           <div id='eight'>
-            <img src='https://cdn.discordapp.com/attachments/533906819324837944/720641274281197648/alina4.jpg' />
+            <img
+              src='https://cdn.discordapp.com/attachments/533906819324837944/720641274281197648/alina4.jpg'
+              alt=''
+            />
           </div>
           <div id='nine'>
-            <img src='https://cdn.discordapp.com/attachments/533906819324837944/723534775994024008/unknown.png' />
+            <img
+              src='https://cdn.discordapp.com/attachments/533906819324837944/723534775994024008/unknown.png'
+              alt=''
+            />
           </div>
         </div>
         <div id='ten'>
-          <img src='https://cdn.discordapp.com/attachments/533906819324837944/723535976714076201/unknown.png' />
+          <img
+            src='https://cdn.discordapp.com/attachments/533906819324837944/723535976714076201/unknown.png'
+            alt=''
+          />
         </div>
         <div id='tennn'>
-          <img src='https://cdn.discordapp.com/attachments/533906819324837944/723536901675417611/unknown.png' />
+          <img
+            src='https://cdn.discordapp.com/attachments/533906819324837944/723536901675417611/unknown.png'
+            alt=''
+          />
         </div>
         <div id='tenn'>
-          <img src='https://cdn.discordapp.com/attachments/533906819324837944/723537013550350456/unknown.png' />
+          <img
+            src='https://cdn.discordapp.com/attachments/533906819324837944/723537013550350456/unknown.png'
+            alt=''
+          />
         </div>
         <div id='tennn'>
-          <img src='https://cdn.discordapp.com/attachments/533906819324837944/723537143321853952/unknown.png' />
+          <img
+            src='https://cdn.discordapp.com/attachments/533906819324837944/723537143321853952/unknown.png'
+            alt=''
+          />
         </div>
       </IonContent>
     </IonPage>
